58cf838f65d35252847bfdc65d3d414e5a3a5e1b
YasserAyman <61660390+YasserAyman@users.noreply.github.com>
2020-08-14 17:54:06 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    var menuToggle = document.querySelector('.menuToggle');
+    var nav = document.querySelector('.navegition');
+    menuToggle.classList.toggle('active');
+    nav.classList.toggle('active')
+}
